2be4d1fc67143091066107c2ba09d3464e353fac
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-01-31 22:39:49 +0100

    32Events example

---
+++ 32Events/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32Events
+{
+    class Class1
+    {
+        public event EventHandler OnPropertyChanged;
+
+        string name = "";
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                //Megvaltozott a property, dobunk most egy esemenyt
+                OnPropertyChanged(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ 32Events/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32Events
+{
+    public partial class Form1 : Form
+    {
+        Class1 myClass = new Class1();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Feliratkozunk az esemenyre egy fgv-nyel. Ezt fog lefutni, amikor a esemenyt dobunk.
+            myClass.OnPropertyChanged += MyClass_OnPropertyChanged;
+
+            myClass.Name = "Zoli";
+        }
+
+        private void MyClass_OnPropertyChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show($"The property has changed. Current value is: {myClass.Name}");
+        }
+    }
+}
