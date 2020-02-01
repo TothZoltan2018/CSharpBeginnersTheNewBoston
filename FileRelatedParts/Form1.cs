9391728361529e958febfd9ea7b4ece573e2acec
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-01 22:27:21 +0100

    37StreamReader2

--- FileRelatedParts/Form1.cs
+++ FileRelatedParts/Form1.cs
@@ -21,12 +21,26 @@ namespace FileRelatedParts
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
- //           ofd.Title = "Open Image";
- //           ofd.Filter = "JPG Image|*jpg|PNG Image|*.png";
+
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
-                textBox1.Text = sr.ReadToEnd();
+                //A fajlban levo elso byte-ot adja vissza tizes szamrendszerben
+                //textBox1.Text = sr.BaseStream.ReadByte().ToString();
+                //A fajlban levo elso byte-ot adja vissza hexadecimalis szamrendszerben
+                //textBox1.Text = sr.BaseStream.ReadByte().ToString("x");
+
+                //A fajlban levo tobb byte-ot olvasunk bufferbe
+                //Kezdopozicio megadasa: pl. 4-es index, azaz az 5. elem
+                //sr.BaseStream.Position = 4;
+                byte[] myBuffer = new byte[6];
+                //A bufferbe tegye a 2. helytol kezdve (1-es index) 3 db byte-ot.
+                sr.BaseStream.Read(myBuffer, 2, 3);
+                foreach (var aByte in myBuffer)
+                {
+                    textBox1.Text += aByte.ToString("x") + " ";
+                }
+                
                 sr.Dispose();
             } 
         }
