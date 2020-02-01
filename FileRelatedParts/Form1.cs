621abf211aefa486db75ab6721416f8775d61dc2
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-01 08:19:05 +0100

    36StreamReader1

--- FileRelatedParts/Form1.cs
+++ FileRelatedParts/Form1.cs
@@ -7,10 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
-/// <summary>
-/// Part 34
-/// </summary>
 namespace FileRelatedParts
 {
     public partial class Form1 : Form
@@ -23,11 +21,13 @@ namespace FileRelatedParts
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Title = "Open Image";
-            ofd.Filter = "JPG Image|*jpg|PNG Image|*.png";
+ //           ofd.Title = "Open Image";
+ //           ofd.Filter = "JPG Image|*jpg|PNG Image|*.png";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                MessageBox.Show($"Successfully open: {ofd.FileName}.");
+                StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+                textBox1.Text = sr.ReadToEnd();
+                sr.Dispose();
             } 
         }
     }
