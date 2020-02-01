7279f951b3d5e27b2bd7c21e93631f7701093a32
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-01 07:41:46 +0100

    34OpenFileDialog

---
+++ FileRelatedParts/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Part 34
+/// </summary>
+namespace FileRelatedParts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Image";
+            ofd.Filter = "JPG Image|*jpg|PNG Image|*.png";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show($"Successfully open: {ofd.FileName}.");
+            } 
+        }
+    }
+}
