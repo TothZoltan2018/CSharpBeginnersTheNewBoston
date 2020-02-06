96adefa9a35c2ccf2e57314bc5399bd2cba28d4d
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-06 21:30:00 +0100

    49Substrings, 50IndexOf and Trim, 51Remove and Replace, 52Split and ToCharArray

--- ArraysAndStrings/Form1.cs
+++ ArraysAndStrings/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace ArraysAndStrings
 {
-    //Part49
+    //Part49, 50, 
     public partial class Form1 : Form
     {
         public Form1()
@@ -20,11 +20,37 @@ namespace ArraysAndStrings
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string Name = "Wolfgang Amadeus Mozart";
-            string FirstName = Name.Substring(0, 8);
+            //Part49 Substrings
+            //string name = "Wolfgang Amadeus Mozart";
+            //string FirstName = name.Substring(0, 8);                        
+            //MessageBox.Show(FirstName);
 
-            MessageBox.Show(FirstName);
+            //Part50 IndexOf and Trim
+            //string name = "      Wolfgang Amadeus Mozart     ";
+            //string trimmedName = name.Trim(); //name.TrimStart() TrimeEnd
+            //string FirstName = trimmedName.Substring(0, trimmedName.IndexOf(' '));            
+            //string MiddleAndLastName = trimmedName.Substring(trimmedName.IndexOf(' ') + 1);
+            //string MiddleName = MiddleAndLastName.Substring(0, MiddleAndLastName.IndexOf(' '));
+            //string LastName = MiddleAndLastName.Substring(MiddleAndLastName.IndexOf(' ') + 1);
+            //MessageBox.Show(FirstName + "\n" + MiddleName + "\n" + LastName);
 
+            //Part51 Remove and Replace
+            //string sentence = "Hello, my name is Zoli";
+            //string removedHello = sentence.Remove(0, 7);
+            //string removedZoli = sentence.Remove(17);
+            //string replacedHello = sentence.Replace("Hello", "Hi");
+            //MessageBox.Show(removedHello + "\n" + removedZoli + "\n" + replacedHello); //karkterekkel is tudja ('', '')
+
+            //Part52 Split and ToCharArray            
+            string names = "Zolika,Sugi, Rege ,Zoli;Aranka; Ica ;Dedi";
+            char[] separatorChars = { ',', ' ', ';' };
+            string[] nameArray = names.Split(separatorChars, StringSplitOptions.RemoveEmptyEntries);
+            //string[] separatorStrings = { ",", ", ", " ,", ";", "; ", " ;" };
+            //string[] nameArray = names.Split(separatorStrings, StringSplitOptions.RemoveEmptyEntries);//Ez sokoldalubb
+            //foreach (string name in nameArray) MessageBox.Show(name);
+
+            string _letters = "abcdefghijkl";
+            char[] letters = _letters.ToCharArray();
         }
     }
 }
