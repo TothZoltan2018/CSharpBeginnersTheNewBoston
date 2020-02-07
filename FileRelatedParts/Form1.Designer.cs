b342fafe7ad502b5517cce77697e1b15003554cc
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-07 22:39:53 +0100

    56, 57, 58, 59 Directory class + AllKindsOfStuff project added (yet empty)

--- FileRelatedParts/Form1.Designer.cs
+++ FileRelatedParts/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -46,14 +48,32 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(38, 120);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
             this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(41, 230);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(251, 20);
+            this.maskedTextBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 202);
+            this.ClientSize = new System.Drawing.Size(362, 414);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +87,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
 
--- FileRelatedParts/Form1.cs
+++ FileRelatedParts/Form1.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace FileRelatedParts
 {
-    //Part47- Is, as and Casting
+    //Part56 FolderBrowsingDialog, Part57 Directory class1, Part58 Directory class2, Part59 Directory class3
     public partial class Form1 : Form
     {
         public Form1()
@@ -21,26 +21,37 @@ namespace FileRelatedParts
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //example1
-            //object myObj = "Zoli";
-            //if (myObj is string) MessageBox.Show((string)myObj);
-
-            //example2
-            Control myControl = button1;
-            if (myControl is Button)
+            //Part56, 57
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "A zoxigen kihasznalasara a vizbul veszi ki a zoxigent, teccikerteni?";
+            fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+            if (fbd.ShowDialog() == DialogResult.OK)
             {
-                //Button myButton = (Button)myControl;
-                //A castolas alternativaja az "as" operator
-                Button myButton = myControl as Button;
-                MessageBox.Show(myButton.Text);
+                //textBox1.Text = fbd.SelectedPath;
+                string[] files = Directory.GetFiles(fbd.SelectedPath);//Directory.GetDirectories, Directory.GetLogicalDrives
+                foreach (var file in files)
+                {
+                    textBox1.Text += file + "\n";
+                    label1.Text += file + "\n";
+                }
+
+                //Part58
+                maskedTextBox1.Text = Directory.GetCreationTime(fbd.SelectedPath).ToString(); //sok egyeb idoinfo is van meg.
+                MessageBox.Show(Directory.GetParent(fbd.SelectedPath).ToString());
+
+                //Part59
+                Directory.CreateDirectory(fbd.SelectedPath + "\\ZoliTest\\Source");
+                //Directory.Exists(fbd.SelectedPath + "\\ZoliTest\\Destination");
+                try
+                {
+                    Directory.Move(fbd.SelectedPath + "\\ZoliTest\\Source", fbd.SelectedPath + "\\ZoliTest\\Destination");
+                }
+                catch (Exception eDir)
+                {
+                    MessageBox.Show(eDir.Message.ToString());
+                }
+                //Directory.Delete() - ures vagy mindent, rekurzivan
             }
-
-
-            //example
-            //example
-            //example
-
-
         }
     }
 }
