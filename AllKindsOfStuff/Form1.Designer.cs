5cdb1a78c736d7b34893dc9b6aaa4b88bcf8856e
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-09 07:48:13 +0100

    Part72 Webclient DownloadString, Part73 Download files

--- AllKindsOfStuff/Form1.Designer.cs
+++ AllKindsOfStuff/Form1.Designer.cs
@@ -28,129 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(18, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Freeze";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(64, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "On";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(10, 50);
+            this.textBox1.Location = new System.Drawing.Point(35, 48);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(171, 356);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(363, 229);
+            this.textBox1.TabIndex = 0;
             // 
-            // groupBox1
+            // button1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 20);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(111, 49);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Threading Part1";
+            this.button1.Location = new System.Drawing.Point(539, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // groupBox2
+            // label1
             // 
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(139, 20);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(194, 416);
-            this.groupBox2.TabIndex = 4;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Threading Part2";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
-            // groupBox3
+            // progressBar1
             // 
-            this.groupBox3.Controls.Add(this.textBox2);
-            this.groupBox3.Controls.Add(this.button3);
-            this.groupBox3.Location = new System.Drawing.Point(369, 20);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(209, 395);
-            this.groupBox3.TabIndex = 5;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Threading Part3";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(71, 18);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "On";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(23, 50);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(166, 355);
-            this.textBox2.TabIndex = 1;
+            this.progressBar1.Location = new System.Drawing.Point(539, 152);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(605, 437);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(732, 372);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -8,77 +8,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Net;//
 
 namespace AllKindsOfStuff
 {
-    //Part69, 70, 71 Threading
+    //Part72 Webclient DownloadString, Part73 Download files
     public partial class Form1 : Form
     {        
-        Thread t1; //Part69
-        Thread t2; //Part70
-        Thread t3; //Part71
-
         public Form1()
         {
             InitializeComponent();
-        }        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Part72
+            //WebClient wc = new WebClient();
+            //textBox1.Text = wc.DownloadString("https://pastebin.com/index/cgKiziiQ");   
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            t1 = new Thread(Freeze);
-            t1.Start();
+            //Part73
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFileAsync(new Uri("https://drive.google.com/open?id=1hwJ3nR_mFTy2ditwBFEMtL-IDUhaHNYs"), sfd.FileName);
+                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;//ugyanez: new AsyncCompletedEventHandler(Wc_DownloadFileCompleted);
+                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+            }
         }
 
-        private void Freeze()
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
-            for (; ; );
+            label1.Text = "Progress: " + e.ProgressPercentage.ToString();
+            progressBar1.Value = e.ProgressPercentage;
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
-            if (t1 != null) t1.Abort();
-            if (t2 != null) t2.Abort();
-            if (t3 != null) t3.Abort();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            t2 = new Thread(Write);
-            t2.Start();
-            //Itt varunk, hogy vegezzen a Write() fgv.
-            //while(t2.IsAlive);
-            t2.Join();
-            textBox1.Text = stringForTheTextBox1;
-        }
-
-        string stringForTheTextBox1 = string.Empty;        
-        void Write()
-        {
-            for (int i = 0; i < 2000; i++)            
-                //System.InvalidOperationException: 'Cross-thread operation not valid: Control 'textBox1'
-                //accessed from a thread other than the thread it was created on.'
-                //textBox1.Text += "Zoli" + i.ToString() + Environment.NewLine;
-                //Nem ferhetunk az uj threadbol a textbox1-hez, ezert tesszuk egy valtozoba.
-                stringForTheTextBox1 += "Zoli" + i.ToString() + Environment.NewLine;            
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            t3 = new Thread(WriteWithParams);
-            //object[] objArray = { "Sugika", 500 };
-            t3.Start(new object[] { "Sugika", 500 });
-            //Itt varunk, hogy vegezzen a WriteWithParams() fgv.
-            //while (t3.IsAlive);
-            t3.Join();
-            textBox2.Text = stringForTheTextBox2;
-        }
-
-        string stringForTheTextBox2 = string.Empty;
-        void WriteWithParams(object myParamArray)
-        {//Csak egyetlen object parametert fogadhat, ezert castolni kell az objectet object tombbe
-            object[] o = (object[])myParamArray;
-            for (int i = 0; i < Convert.ToInt32(o[1]); i++)
-                stringForTheTextBox2 += o[0] + i.ToString() + Environment.NewLine;            
+            MessageBox.Show("File is downloaded.");
         }
     }
 }
