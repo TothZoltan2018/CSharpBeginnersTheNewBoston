5dd9632847f6a6092a67011bf2244afc0a5cb9ac
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-15 18:41:11 +0100

    Part99, 100, 101 TreeView Control

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -11,27 +11,98 @@ using System.Media;
 
 namespace AllKindsOfStuff
 {
-    //Part98 Save Project Settings.
+    //
     public partial class Form1 : Form
-    {        
+    {
         public Form1()
         {
             InitializeComponent();
-            //A menuben felvenni az elmentendo valtozokat: Project-->AllKindsOfStuff Properties --> Settings
-            //Visszaolvasas
-            textBox1.Text = AllKindsOfStuff.Properties.Settings.Default.nameTobePreserved;
-            textBox2.Text = AllKindsOfStuff.Properties.Settings.Default.ageTobePreserved.ToString();
-            button1 = AllKindsOfStuff.Properties.Settings.Default.myButton;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Kimentes
-            AllKindsOfStuff.Properties.Settings.Default.nameTobePreserved = textBox1.Text;
-            AllKindsOfStuff.Properties.Settings.Default.ageTobePreserved = Convert.ToInt32(textBox2.Text);
-            AllKindsOfStuff.Properties.Settings.Default.myButton = button1;
+            treeView1.Nodes.Add("People"); //ez a fa egyik gyokere --> treeView1.Nodes[0]
+            treeView1.Nodes.Add("Animals"); //ez a fa masik gyokere --> treeView1.Nodes[1]
+            treeView1.Nodes[0].Nodes.Add("Zolika");
+            treeView1.Nodes[0].Nodes.Add("Sugika");
+            treeView1.Nodes[0].Nodes.Add("Rege");
+            treeView1.Nodes[1].Nodes.Add("Dog");
+            treeView1.Nodes[1].Nodes.Add("Cat");
+            treeView1.Nodes[1].Nodes[0].Nodes.Add("Foxhund");
+            treeView1.Nodes[1].Nodes[0].Nodes.Add("Wolf-Dog");
+        }
 
-            AllKindsOfStuff.Properties.Settings.Default.Save();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+        }
+                
+        void RemoveCheckedNodes(TreeNodeCollection tnc)
+        {
+            List<TreeNode> tnListToBeRemoved = new List<TreeNode>();
+            foreach (TreeNode node in tnc)
+                if (node.Checked) tnListToBeRemoved.Add(node);
+                else if (node.Nodes.Count != 0) RemoveCheckedNodes(node.Nodes); //vannak alatta Node-ok, azokra rekurzio
+            foreach (TreeNode node in tnListToBeRemoved)
+            {
+                node.Remove();
+                //treeView1.Nodes.Remove(node); //ugyanaz, mint fentebb
+            }
+         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RemoveCheckedNodes(treeView1.Nodes);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //MyRemoveCheckedNodes(treeView1.Nodes); //Ez kivetelt dob!
+        }
+
+        private void MyRemoveCheckedNodes(TreeNodeCollection tnc)
+        {
+            foreach (TreeNode node in tnc)
+            {
+                if (node.Checked) node.Remove(); //Ez kivetelt dob!
+                else if (node.Nodes.Count != 0) MyRemoveCheckedNodes(node.Nodes);                
+            } 
+        }
+
+        //Part101
+        //1. Rahuzok egz ImageList-et a form-ra
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //2.
+            TreeNode tnPers = new TreeNode();
+            tnPers.Text = "Pers.";
+            tnPers.SelectedImageIndex = tnPers.ImageIndex = 1;            
+            treeView1.Nodes.Add(tnPers);
+            //3. A UI-on A treeView1 propertyknel meg kell adni az imageList-unket/
+            TreeNode tnSugika = new TreeNode();
+            tnSugika.Text = "Sugika";
+            tnSugika.SelectedImageIndex = tnSugika.ImageIndex = 2;
+            treeView1.Nodes[0].Nodes.Add(tnSugika);
+            TreeNode tnZolika = new TreeNode();
+            tnZolika.Text = "Sugika";
+            tnZolika.SelectedImageIndex = tnZolika.ImageIndex = 3;
+            treeView1.Nodes[0].Nodes.Add(tnZolika);
+
+            //TreeNode tnAnimals = new TreeNode();
+            //tnAnimals.Text = "Animals";
+            //treeView1.Nodes.Add(tnAnimals);
+            treeView1.Nodes.Add("Animals");
+            treeView1.Nodes[1].Nodes.Add("Dogs");          
+            TreeNode tnWolfDog = new TreeNode();
+            tnWolfDog.Text = "Wolf-Dog";
+            tnWolfDog.ImageIndex = 4;
+            tnWolfDog.SelectedImageIndex = 4;
+            treeView1.Nodes[1].Nodes[0].Nodes.Add(tnWolfDog);
         }
     }
 }
