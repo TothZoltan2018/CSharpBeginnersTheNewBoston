f98174a33457e86f49799da1f3f399552e6389fd
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-18 23:11:33 +0100

    Part115 - MD5 and SHA1

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Media;
+using System.Security.Cryptography;//
 
 namespace AllKindsOfStuff
 {
-    
+    //Part115 - MD5 and SHA1
     public partial class Form1 : Form
     {
         public Form1()
@@ -21,7 +21,14 @@ namespace AllKindsOfStuff
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            string md5Result = BitConverter.ToString(md5.ComputeHash(utf8.GetBytes(textBox1.Text)));
+
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+            string sha1Result = BitConverter.ToString(sha1.ComputeHash(utf8.GetBytes(textBox1.Text)));
             
+            MessageBox.Show(md5Result + Environment.NewLine + sha1Result);
         }
     }
 }
