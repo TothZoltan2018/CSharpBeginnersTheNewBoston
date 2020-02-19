c9da12f8e4a177a94f6f365adc11736f02195e93
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-19 22:21:38 +0100

    Part116, 117 - TripleDES Encryption/Decryption

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -11,24 +11,51 @@ using System.Security.Cryptography;//
 
 namespace AllKindsOfStuff
 {
-    //Part115 - MD5 and SHA1
+    //Part116, 117 - TripleDES Encryption/Decryption
     public partial class Form1 : Form
     {
+        TripleDESCryptoServiceProvider tDes;
+        UTF8Encoding utf8;
+        byte[] encrypted;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void SetupCryption(TextBox tbox)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            utf8 = new UTF8Encoding();
+
+            tDes = new TripleDESCryptoServiceProvider();
+            tDes.Key = md5.ComputeHash(utf8.GetBytes(tbox.Text));
+            //A decrypteleshez kell ez a ket sor:
+            tDes.Mode = CipherMode.ECB;
+            tDes.Padding = PaddingMode.PKCS7;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            UTF8Encoding utf8 = new UTF8Encoding();
-            string md5Result = BitConverter.ToString(md5.ComputeHash(utf8.GetBytes(textBox1.Text)));
-
-            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
-            string sha1Result = BitConverter.ToString(sha1.ComputeHash(utf8.GetBytes(textBox1.Text)));
-            
-            MessageBox.Show(md5Result + Environment.NewLine + sha1Result);
+            SetupCryption(textBox1);
+            ICryptoTransform trans = tDes.CreateEncryptor();
+            //ez vegzi el tenylegesen a titkositast a textBox2.Text szovegen.
+            encrypted = trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length);
+            textBox3.Text = BitConverter.ToString(encrypted);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetupCryption(textBox4);
+            ICryptoTransform trans = tDes.CreateDecryptor();
+            try
+            {
+                textBox5.Text = utf8.GetString(trans.TransformFinalBlock(encrypted, 0, encrypted.Length));
+            }
+            catch (Exception exce)
+            {
+                MessageBox.Show("Ooooo!" + exce.Message);
+            }            
+        }  
     }
 }
