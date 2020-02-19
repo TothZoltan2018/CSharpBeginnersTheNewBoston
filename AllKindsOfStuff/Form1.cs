c83e0ea9aede9aa7fc8f2f34e9a2c07cdcd0a693
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-19 22:46:23 +0100

    118 - Drag and Drop

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -11,51 +11,26 @@ using System.Security.Cryptography;//
 
 namespace AllKindsOfStuff
 {
-    //Part116, 117 - TripleDES Encryption/Decryption
+    //118 - Drag and Drop
     public partial class Form1 : Form
     {
-        TripleDESCryptoServiceProvider tDes;
-        UTF8Encoding utf8;
-        byte[] encrypted;
 
         public Form1()
         {
             InitializeComponent();            
         }
 
-        private void SetupCryption(TextBox tbox)
+        private void panel1_DragOver(object sender, DragEventArgs e)
         {
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            utf8 = new UTF8Encoding();
-
-            tDes = new TripleDESCryptoServiceProvider();
-            tDes.Key = md5.ComputeHash(utf8.GetBytes(tbox.Text));
-            //A decrypteleshez kell ez a ket sor:
-            tDes.Mode = CipherMode.ECB;
-            tDes.Padding = PaddingMode.PKCS7;
+            e.Effect = DragDropEffects.All;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void panel1_DragDrop(object sender, DragEventArgs e)
         {
-            SetupCryption(textBox1);
-            ICryptoTransform trans = tDes.CreateEncryptor();
-            //ez vegzi el tenylegesen a titkositast a textBox2.Text szovegen.
-            encrypted = trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length);
-            textBox3.Text = BitConverter.ToString(encrypted);
+            //A behuzott fajlok neve:
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            foreach (string file in files)
+                    MessageBox.Show(file);            
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            SetupCryption(textBox4);
-            ICryptoTransform trans = tDes.CreateDecryptor();
-            try
-            {
-                textBox5.Text = utf8.GetString(trans.TransformFinalBlock(encrypted, 0, encrypted.Length));
-            }
-            catch (Exception exce)
-            {
-                MessageBox.Show("Ooooo!" + exce.Message);
-            }            
-        }  
     }
 }
