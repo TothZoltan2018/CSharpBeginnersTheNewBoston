c84aac736d4f27b15ea06d7e272dbed5fb83ca52
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-27 21:52:33 +0100

    Part145 - Capturing Screen + kommentek

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -29,9 +29,13 @@ namespace AllKindsOfStuff
         {
             for (; ; ) //endless loop
             {
+                //Ebben taroljuk majd a screen capture-t
                 Bitmap b = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+                //Ez fogja kirajzolni a screen capture-t
                 Graphics g = Graphics.FromImage(b);
+                //Ez csinalja a screen capture-t a Bitmap b-be
                 g.CopyFromScreen(Point.Empty, Point.Empty, Screen.PrimaryScreen.WorkingArea.Size);
+                //Megjelenitjuk a pictureBox-ban
                 pictureBox1.Image = b;
             }
         }
