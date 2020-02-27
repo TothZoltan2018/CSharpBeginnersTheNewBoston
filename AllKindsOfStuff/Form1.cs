1eae554d54ffeee2da23beb18afe28fc2703faa9
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-02-27 21:06:40 +0100

    Part144 - Goto Keyword and Regions

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -7,15 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Security.Cryptography;//
+using System.Security.Cryptography;
 
 namespace AllKindsOfStuff
-{    
+{    //Part144 - Goto Keyword and Regions
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region endless loop with gotos
+            goto MyCode;
+        LabelBeforeSwitch:
+            {
+                string Zoli = "bbbbbbb";
+                switch (Zoli)
+                {
+                    case "Zoli":
+                        MessageBox.Show("Hello");
+                        break;
+                    default:
+                        MessageBox.Show("The default");
+                        goto case "Zoli";
+                }
+            }
+        MyCode:
+        //{} nem szukseges
+                MessageBox.Show("This is printed at label MyCode");
+                goto LabelBeforeSwitch;
+            
+            #endregion
         }
     }
 }
--- AllKindsOfStuff/Program.cs
+++ AllKindsOfStuff/Program.cs
@@ -16,7 +16,7 @@ namespace AllKindsOfStuff
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SplashScrreen());
+            Application.Run(new Form1());
         }
     }
 }
