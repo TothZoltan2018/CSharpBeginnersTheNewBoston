6d3101167b32582491b7d00555041e2edcf4b3e3
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-08 07:05:56 +0100

    Part178 - Preprocessor Directives

--- FileRelatedParts/Form1.Designer.cs
+++ FileRelatedParts/Form1.Designer.cs
@@ -30,17 +30,29 @@
         {
             this.components = new System.ComponentModel.Container();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // toolTip1
             // 
             this.toolTip1.IsBalloon = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 64);
+            this.ClientSize = new System.Drawing.Size(435, 354);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -49,6 +61,7 @@
 
         #endregion
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FileRelatedParts/Form1.cs
+++ FileRelatedParts/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿#define Valami
+//#undef Valami
+#define Akarmi
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,51 +16,33 @@ using System.Xml;
 
 namespace FileRelatedParts
 {
-    //Part176 - Notified When Files Change
+    //Part178 - Preprocessor Directives
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
-
-            //FileSystemWatcher fsw = new FileSystemWatcher();
-            //fsw.Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            //fsw.NotifyFilter = NotifyFilters.LastWrite;
-            //fsw.Filter = "*.txt";
-
-            //Event, ami figyeli a fentieket
-            //fsw.Changed += Fsw_Changed;
-            //fsw.EnableRaisingEvents = true;
-
-            //tobb kiterjesztes figyelese (a stackoverflow.com-rol)
-            //Az egyes filtereknek kulon FileSystemWatcher-t hozunk letre.
-            string[] filters = { "*.txt", "*.text" };
-            //List<FileSystemWatcher> fswList = new List<FileSystemWatcher>();
-
-            foreach (string filter in filters)
-            {
-                FileSystemWatcher fsw = new FileSystemWatcher();
-                fsw.Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                //szukithetjuk, hogz milyen esemenytipusokat figyrljen. Ha egyet mar megadunk, akkor
-                //csak a felsoroltak lesznek engedelyezve:
-                //fsw.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.LastAccess;
-                fsw.EnableRaisingEvents = true; //bekapcsoljuk a figyelest
-                fsw.Filter = filter; //az ilyen fajl kiterjesztesre figyeljen.
-                //minden egyes esemenyt ugyanazok az eventhandler-ek szolgalnak ki.
-                fsw.Changed += Fsw_Changed;                
-                fsw.Renamed += Fsw_Renamed;                
-                //fswList.Add(fsw); //E nelkul is jol mukodik. Minek oket listaban tarolni?                
-            }            
+            InitializeComponent();               
         }
 
-        private void Fsw_Renamed(object sender, RenamedEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("A textfile has been renamed: " + e.Name);
-        }
+            #region Valami
+            //
+            //
+            //
+            #endregion
 
-        private void Fsw_Changed(object sender, FileSystemEventArgs e)
-        {
-            MessageBox.Show("You have saved a textfile " + e.Name);
+#if Valami
+            MessageBox.Show("Valami is #defined");
+#warning Valami is defined
+#elif Akarmi
+            MessageBox.Show("Valami is #defined");
+#else
+            MessageBox.Show("Semmi is #defined
+#error Semmi is defined
+#endif //Valami
+
+
         }
     }
 }
