d25a8d306d668908ff3be5045f65d2354058090a
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-15 06:43:03 +0100

    Part198 - IDisposable pt 2

--- AllKindsOfStuff/Form1.cs
+++ AllKindsOfStuff/Form1.cs
@@ -13,7 +13,7 @@ using System.Collections;
 
 namespace AllKindsOfStuff
 {
-    //Part197 - IDisposable pt 1
+    //Part198 - IDisposable pt 2
     public partial class Form1 : Form
     {
         public Form1()
@@ -23,21 +23,39 @@ namespace AllKindsOfStuff
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.IO.BinaryReader br = new System.IO.BinaryReader(System.IO.File.OpenRead(""));
-            br.Dispose(); //Calls the destructor
+            using (MyClass mc = new MyClass())
+            {
+                
+            }//Itt dispozalja az mc objektumot
         }
     }
 
-    class MyClass
+    class MyClass : IDisposable
     {
+        Image i;
         public MyClass()
         {
-            MessageBox.Show("I am the constructor.");
+            i = Image.FromFile(@"C:\Users\ZoliRege\Documents\7AB6683EEA48580398F2ECD1E3B74964.png");
+        }
+
+        //Ezt hivja a using blokk
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); //A destruktora nem lesz meghivva
+        }
+
+        protected virtual void Dispose(bool b)
+        {
+            if (b)
+            {
+                i.Dispose();
+            }
         }
 
         ~MyClass()
         {
-            MessageBox.Show("I am the destructor.");
+            MessageBox.Show("Engem soha nem hivnak meg, a \" GC.SuppressFinalize(this);\" sor miatt!");
         }
     }
 }
